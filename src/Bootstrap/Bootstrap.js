88437a67bff8d95cb6f3fe0f8ca3769d654c2512
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-18 23:46:49 +0530

    button

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -4,178 +4,63 @@ function App() {
   return (
     <>
       <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">Text Formatting lead</h1>
-          </div>
-        </div>
+        <h2 className="text-center mt-3 mb-5 fs-4">Bootstrap Button Classes</h2>
         <div className="row">
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
+          <div className="col py-3">
+            <a href="" className="btn btn-primary">
+              Submit
+            </a>
+            <a href="" className="btn btn-secondary">
+              Submit
+            </a>
+            <a href="" className="btn btn-success">
+              Submit
+            </a>
+            <button type="button" className="btn btn-warning">
+              Submit
+            </button>
+            <a href="" className="btn btn-outline-info">
+              Submit
+            </a>
+            <a href="" className="btn btn-outline-dark">
+              Submit
+            </a>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">Unordered List</h1>
+        <h2 className="text-center mt-3 mb-5 fs-4">Bootstrap Button Classes</h2>
+        <div className="row">
+          <div className="col py-3">
+            <a href="" className="btn btn-primary btn-lg active">
+              Submit
+            </a>
+            <a href="" className="btn btn-secondary ">
+              Submit
+            </a>
+            <a href="" className="btn btn-success btn-sm">
+              Submit
+            </a>
+            <button type="button" className="btn btn-warning">
+              Submit
+            </button>
+            <a href="" className="btn btn-outline-info">
+              Submit
+            </a>
+            <a href="" className="btn btn-outline-dark">
+              Submit
+            </a>
           </div>
         </div>
         <div className="row">
-          <div className="col-md-4">
-            <ul>
-              <li>Orange</li>
-              <li>Apple</li>
-              <li>Banana</li>
-              <li>Grapes</li>
-            </ul>
-          </div>
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">Removing Bullets</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <ul className="list-unstyled">
-              <li>Orange</li>
-              <li>Apple</li>
-              <li>Banana</li>
-              <li>Grapes</li>
-            </ul>
-          </div>
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">Inline list</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <ul className="list-unstyled list-inline">
-              <li className="list-inline-item">Orange</li>
-              <li className="list-inline-item">Apple</li>
-              <li className="list-inline-item">Banana</li>
-              <li className="list-inline-item">Grapes</li>
-            </ul>
-          </div>
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">Inline list</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4">
-            <ul className="list-unstyled list-inline">
-              <li className="list-inline-item">Orange</li>
-              <li className="list-inline-item">Apple</li>
-              <li className="list-inline-item">Banana</li>
-              <li className="list-inline-item">Grapes</li>
-            </ul>
-          </div>
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12">
-            <blockquote className="blockquote">
-              <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, deleniti expedita esse nisi totam reiciendis!"</p>
-              <footer className="blockquote-footer">Yahoo baba</footer>
-            </blockquote>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">User Select All</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead user-select-all">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">User Select None</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead user-select-none">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <div className="row py-2">
-          <div className="col-md-12">
-            <h1 className="text-center fs-4">User Select Auto</h1>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-4 mx-auto pink">
-            <h2 className="text-danger">Sub Heading</h2>
-            <p className="lead user-select-auto">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea iure
-              obcaecati id aliquid saepe voluptatibus incidunt aperiam quidem
-              aspernatur repellat.
-            </p>
+          <div className="col-md-6 py-3">
+            <button className="btn btn-primary btn-lg btn-block">Submit</button>
+            <a href="" className="btn btn-primary active">
+              Submit
+            </a>{" "}
+            <a href="" className="btn disabled btn-secondary">
+              Submit
+            </a>
           </div>
         </div>
       </div>
