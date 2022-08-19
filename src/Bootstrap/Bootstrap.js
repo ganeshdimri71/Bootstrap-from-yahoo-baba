ae7e8a8b0684ffccf1d5cb502fb5e7536fdab116
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-19 19:18:59 +0530

    list-grou

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -6,222 +6,188 @@ function App() {
   return (
     <>
       <div className="container mb-4">
-        <h2 className="text-center mt-3 mb-5 fs-4">Bootstrap Button Classes</h2>
+        <h2 className="text-center mt-3 mb-5 fs-4">
+          Bootstrap List Group Classes
+        </h2>
         <div className="row">
-          <div className="col py-3">
-            <div className="btn-group">
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="text-center mt-3 mb-5 fs-4">Bootstrap Button Classes</h2>
-        <div className="row">
-          <div className="col py-3">
-            <div className="btn-group">
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-              <button className="btn btn-success" type="button">
-                Click
-              </button>
-              <button className="btn btn-danger" type="button">
-                Click
-              </button>
-            </div>
+          <div className="col-sm-5 py-3">
+            <ul className="list-group">
+              <li className="list-group-item">List Item One</li>
+              <li className="list-group-item">List Item Two</li>
+              <li className="list-group-item">List Item Three</li>
+              <li className="list-group-item">List Item Four</li>
+              <li className="list-group-item">List Item Five</li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="container mb-4">
         <h2 className="text-center mt-3 mb-5 fs-4">
-          Bootstrap Button Classes Sizes
+          Bootstrap List Group Classes
         </h2>
         <div className="row">
-          <div className="col py-3">
-            <div className="btn-group-lg">
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-              <button className="btn btn-success" type="button">
-                Click
-              </button>
-              <button className="btn btn-danger" type="button">
-                Click
-              </button>
-            </div>
+          <div className="col-sm-5 py-3">
+            <ul className="list-group-flush">
+              <li className="list-group-item">List Item One</li>
+              <li className="list-group-item">List Item Two</li>
+              <li className="list-group-item">List Item Three</li>
+              <li className="list-group-item">List Item Four</li>
+              <li className="list-group-item">List Item Five</li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="container mb-4">
         <h2 className="text-center mt-3 mb-5 fs-4">
-          Bootstrap Button Classes Small
+          Bootstrap List Group Classes
         </h2>
         <div className="row">
-          <div className="col py-3">
-            <div className="btn-group-sm">
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-              <button className="btn btn-success" type="button">
-                Click
-              </button>
-              <button className="btn btn-danger" type="button">
-                Click
-              </button>
-            </div>
+          <div className="col-sm-5 py-3">
+            <ul className="list-group">
+              <li className="list-group-item active">List Item One</li>
+              <li className="list-group-item">List Item Two</li>
+              <li className="list-group-item">List Item Three</li>
+              <li className="list-group-item">List Item Four</li>
+              <li className="list-group-item disabled">List Item Five</li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="container mb-4">
         <h2 className="text-center mt-3 mb-5 fs-4">
-          Bootstrap Button Classes Normal Size
+          Bootstrap List Group Classes
         </h2>
         <div className="row">
-          <div className="col py-3">
-            <div className="btn-group">
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-              <button className="btn btn-success" type="button">
-                Click
-              </button>
-              <button className="btn btn-danger" type="button">
-                Click
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="text-center mt-3 mb-5 fs-4">Bootstrap Button Toolbar</h2>
-        <div className="row">
-          <div className="col py-3">
-            <div className="btn-group-vertical">
-              <button className="btn btn-primary" type="button">
-                Click
-              </button>
-              <button className="btn btn-success" type="button">
-                Click
-              </button>
-              <button className="btn btn-danger" type="button">
-                Click
-              </button>
-              <button className="btn btn-info" type="button">
-                Click
-              </button>
-              <button className="btn btn-warning" type="button">
-                Click
-              </button>
-              <button className="btn btn-light" type="button">
-                Click
-              </button>
-            </div>
+          <div className="col-sm-5 py-3">
+            <ul className="list-group">
+              <li className="list-group-item list-group-item-primary">
+                List Item One
+              </li>
+              <li className="list-group-item list-group-item-success">
+                List Item Two
+              </li>
+              <li className="list-group-item list-group-item-warning">
+                List Item Three
+              </li>
+              <li className="list-group-item list-group-item-danger">
+                List Item Four
+              </li>
+              <li className="list-group-item list-group-item-info">
+                List Item Five
+              </li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="container mb-4">
         <h2 className="text-center mt-3 mb-5 fs-4">
-          Bootstrap Button Toolbar Font-awesome Icon
+          Bootstrap List Group Classes
         </h2>
         <div className="row">
-          <div className="col py-3">
-            <div className="btn-toolbar">
-              <div className="btn-group me-3">
-                <button className="btn btn-primary" type="button">
-                  Click
-                </button>
-                <button className="btn btn-success" type="button">
-                  Click
-                </button>
-                <button className="btn btn-danger" type="button">
-                  Click
-                </button>
-                <button className="btn btn-info" type="button">
-                  Click
-                </button>
-                <button className="btn btn-warning" type="button">
-                  Click
-                </button>
-                <button className="btn btn-light" type="button">
-                  Click
-                </button>
-              </div>
-              <div className="btn-group">
-                <button className="btn btn-primary" type="button">
-                  Click
-                </button>
-                <button className="btn btn-success" type="button">
-                  Click
-                </button>
-                <button className="btn btn-danger" type="button">
-                  Click
-                </button>
-                <button className="btn btn-info" type="button">
-                  Click
-                </button>
-                <button className="btn btn-warning" type="button">
-                  Click
-                </button>
-                <button className="btn btn-light" type="button">
-                  Click
-                </button>
-              </div>
-            </div>
+          <div className="col-sm-5 py-3">
+            <ul className="list-group">
+              <li className="list-group-item list-group-item-primary">
+                List Item One
+              </li>
+              <li className="list-group-item list-group-item-success">
+                List Item Two
+              </li>
+              <li className="list-group-item list-group-item-warning">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+                  obcaecati ipsam exercitationem labore, voluptatum tempore
+                  aperiam quibusdam odio iusto praesentium facilis aspernatur
+                  vero eligendi pariatur odit nulla adipisci reiciendis nemo.
+                </p>
+              </li>
+              <li className="list-group-item list-group-item-danger">
+                List Item Four
+              </li>
+              <li className="list-group-item list-group-item-info">
+                List Item Five
+              </li>
+            </ul>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="text-center mt-3 mb-5 fs-4">Bootstrap Button Toolbar</h2>
+        <h2 className="text-center mt-3 mb-5 fs-4">
+          Bootstrap List Group Hrizontal Classes
+        </h2>
         <div className="row">
-          <div className="col py-3">
-              <div className="btn-group">
-                <button className="btn btn-primary" type="button">
-                  Click
-                </button>
-                <button className="btn btn-success" type="button">
-                  Click
-                </button>
-              <div className="btn-group">
-                <a href="" className="btn btn-danger dropdown-toggle" data-toggle="dropdown">Click</a>
-                <div className="dropdown-menu">
-                  <a href="" className="dropdown-item">Web Design</a>
-                  <a href="" className="dropdown-item">Web Development</a>
-                </div>
-            </div>
-              </div>
-              <div className="btn-group">
-                <button className="btn btn-primary" type="button">
-                  <FiFacebook />
-                </button>
-                <button className="btn btn-success" type="button">
-                  <FiFacebook />
-                </button>
-                <button className="btn btn-danger" type="button">
-                  <FiFacebook />
-                </button>
-                <button className="btn btn-info" type="button">
-                  <FiFacebook />
-                </button>
-                <button className="btn btn-warning" type="button">
-                  <FiFacebook />
-                </button>
-                <button className="btn btn-light" type="button">
-                  <FiFacebook />
-                </button>
-              </div>
+          <div className="col-sm-7 py-3">
+            <ul className="list-group list-group-horizontal">
+              <li className="list-group-item">List Item One</li>
+              <li className="list-group-item">List Item Two</li>
+              <li className="list-group-item">List Item Three</li>
+              <li className="list-group-item">List Item Four</li>
+              <li className="list-group-item">List Item Five</li>
+            </ul>
           </div>
         </div>
       </div>
-
+      <div className="container mb-4">
+        <h2 className="text-center mt-3 mb-5 fs-4">list-group-horizontal-lg</h2>
+        <div className="row">
+          <div className="col-sm-7 py-3">
+            <ul className="list-group list-group-horizontal-lg">
+              <li className="list-group-item">List Item One</li>
+              <li className="list-group-item">List Item Two</li>
+              <li className="list-group-item">List Item Three</li>
+              <li className="list-group-item">List Item Four</li>
+              <li className="list-group-item">List Item Five</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="text-center mt-3 mb-5 fs-4">
+          Bootstrap List Group Horrizontal Classes
+        </h2>
+        <div className="row">
+          <div className="col-sm-7 py-3">
+            <ul className="list-group list-group-horizontal-lg">
+              <li className="list-group-item">List Item One</li>
+              <li className="list-group-item">List Item Two</li>
+              <li className="list-group-item">List Item Three</li>
+              <li className="list-group-item">List Item Four</li>
+              <li className="list-group-item">List Item Five</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="text-center mt-3 mb-5 fs-4">
+          Bootstrap List Group Horrizontal Classes
+        </h2>
+        <div className="row">
+          <div className="col-sm-7 py-3">
+            <div className="list-group">
+              <a
+                href="#"
+                className="list-group-item list-group-action list-group-item-primary"
+              >
+                List Item One
+              </a>
+              <a
+                href="#"
+                className="list-group-item list-group-action list-group-item-success"
+              >
+                List Item Two
+              </a>
+              <a className="list-group-item list-group-action">
+                List Item Three
+              </a>
+              <a href="#" className="list-group-item list-group-action">
+                List Item Four
+              </a>
+              <a href="#" className="list-group-item list-group-action">
+                List Item Five
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
