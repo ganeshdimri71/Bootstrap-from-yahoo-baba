9e114de27aec31a44851b6fc211f5778d3f78d07
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-20 22:03:53 +0530

    navbar

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -4,192 +4,135 @@ function App() {
   return (
     <>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap 4 form Classes</h2>
+        <h2 className="py-3 text-center">Bootstrap Nav Classes</h2>
         <div className="row py-3">
-          <div className="col-md-6">
-            <div className="form-group">
-              <label>Name</label>
-              <input type="text" className="form-control" />
-            </div>
-            <div className="form-group">
-              <label>Email</label>
-              <input
-                type="text"
-                value="abc@gmail.com"
-                className="form-control-plaintext"
-                readonly
-              />
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input type="password" className="form-control" />
-              <small className="form-text text-muted">
-                Your Password must be 8-20 characters
-              </small>
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input type="password" className="form-control" />
-            </div>
-            <div className="form-group">
-              <label>Country</label>
-              <select name="" id="" className="form-control">
-                <option value="">India</option>
-                <option value="">Pakisthan</option>
-                <option value="">Bangladesh</option>
-                <option value="">Nepal</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Message</label>
-              <textarea
-                name=""
-                id=""
-                cols="30"
-                rows="10"
-                className="form-control"
-              ></textarea>
-            </div>
-          </div>
-          <div className="col-md-6"></div>
+          <div className="col">
+            <ul className="nav">
+              <li className="nav-item">
+                <a href="" className="nav-link">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">Services</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">Contact Us</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">Gallery</a>
+              </li>
+            </ul>
+           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap 4 Input Sizes</h2>
+        <h2 className="py-3 text-center">Bootstrap Nav Classes Active Disabled styling</h2>
         <div className="row py-3">
-          <div className="col-md-6">
-            <div className="form-group">
-              <label>Name</label>
-              <input type="text" className="form-control form-control-lg" />
-            </div>
-            <div className="form-group">
-              <label>Name</label>
-              <input type="text" className="form-control form-control-sm" />
-            </div>
-            <div className="form-group">
-              <label>Name</label>
-              <input
-                type="text"
-                className="form-control form-control-default"
-              />
-            </div>
-            <div className="form-group">
-              <label>Email</label>
-              <input
-                type="text"
-                value="abc@gmail.com"
-                className="form-control-plaintext"
-                readonly
-              />
-            </div>
-            <div className="form-group">
-              <label>Password</label>
-              <input type="password" className="form-control" />
-              <small className="form-text text-muted">
-                Your Password must be 8-20 characters
-              </small>
-            </div>
-            <div className="form-group">
-              <label>Country</label>
-              <input type="password" className="form-control" />
-            </div>
-            <div className="form-group">
-              <label>Country</label>
-              <select name="" id="" className="form-control">
-                <option value="">India</option>
-                <option value="">Pakisthan</option>
-                <option value="">Bangladesh</option>
-                <option value="">Nepal</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <label>Message</label>
-              <textarea
-                name=""
-                id=""
-                cols="30"
-                rows="10"
-                className="form-control"
-              ></textarea>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="form-group">
-              <label htmlFor="formFile" className="form-label">
-                Default file input example
-              </label>
-              <input className="form-control" type="file" id="formFile" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="customRange1" className="form-label">
-                Example range
-              </label>
-              <input type="range" className="form-range" id="customRange1" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="customRange1" className="form-label">
-                Hobbies
-              </label>
-              <div className="form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="sportCheck"
-                />
-                <label htmlFor="sportCheck" className="form-check-label">
-                  Sports
-                </label>
-              </div>
-              <div className="form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="bookCheck"
-                />
-                <label htmlFor="bookCheck" className="form-check-label">
-                  Books
-                </label>
-              </div>
-              <div className="form-check">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="bookCheck"
-                  disabled
-                />
-                <label htmlFor="bookCheck" className="form-check-label">
-                  Books
-                </label>
-              </div>
-            </div>
-            <div className="form-group">
-              <label htmlFor="customRange1" className="form-label">
-                Gender
-              </label>
-              <div className="form-check">
-                <input
-                  type="radio"
-                  className="form-check-input"
-                  id="maleRadio"
-                  name="gender"
-                />
-                <label htmlFor="maleRadio" className="form-check-label">
-                  Male
-                </label>
-              </div>
-              <div className="form-check">
-                <input
-                  type="radio"
-                  className="form-check-input"
-                  id="femaleRadio"
-                  name="gender"
-                />
-                <label htmlFor="checkbox" className="form-check-label">
-                  Female
-                </label>
-              </div>
-            </div>
-          </div>
+          <div className="col">
+            <ul className="nav">
+              <li className="nav-item">
+                <a href="" className="nav-link active">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">Services</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link">Contact Us</a>
+              </li>
+              <li className="nav-item">
+                <a href="" className="nav-link disabled">Gallery</a>
+              </li>
+            </ul>
+           </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Bootstrap Nav Classes Active Disabled</h2>
+        <div className="row py-3">
+          <div className="col">
+            <nav className="nav">
+            <a href="" className="nav-link">Home</a>
+            <a href="" className="nav-link active">About Us</a>
+            <a href="" className="nav-link">Gallery</a>
+            <a href="" className="nav-link">Blogs</a>
+            <a href="" className="nav-link">Contact Us</a>
+            </nav>
+           </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Bootstrap Nav Classes Center align</h2>
+        <div className="row py-5">
+          <div className="col">
+            <nav className="nav justify-content-center">
+            <a href="" className="nav-link">Home</a>
+            <a href="" className="nav-link active">About Us</a>
+            <a href="" className="nav-link">Gallery</a>
+            <a href="" className="nav-link">Blogs</a>
+            <a href="" className="nav-link">Contact Us</a>
+            </nav>
+           </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Bootstrap Nav Classes right align</h2>
+        <div className="row py-5">
+          <div className="col">
+            <nav className="nav justify-content-end">
+            <a href="" className="nav-link">Home</a>
+            <a href="" className="nav-link active">About Us</a>
+            <a href="" className="nav-link">Gallery</a>
+            <a href="" className="nav-link">Blogs</a>
+            <a href="" className="nav-link">Contact Us</a>
+            </nav>
+           </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Bootstrap Nav Classes Menu Vertical</h2>
+        <div className="row py-5">
+          <div className="col">
+            <nav className="nav flex-column">
+            <a href="" className="nav-link">Home</a>
+            <a href="" className="nav-link active">About Us</a>
+            <a href="" className="nav-link">Gallery</a>
+            <a href="" className="nav-link">Blogs</a>
+            <a href="" className="nav-link">Contact Us</a>
+            </nav>
+           </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Bootstrap Nav Classes Nav Pills</h2>
+        <div className="row py-5">
+          <div className="col">
+            <ul className="nav nav-pills">
+            <a href="" className="nav-link">Home</a>
+            <a href="" className="nav-link active">About Us</a>
+            <a href="" className="nav-link">Gallery</a>
+            <a href="" className="nav-link">Blogs</a>
+            <a href="" className="nav-link">Contact Us</a>
+            </ul>
+           </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Bootstrap Nav Classes Nav Pills And Nav Fills</h2>
+        <div className="row py-5">
+          <div className="col">
+            <ul className="nav nav-pills nav-fill">
+            <a href="" className="nav-link">Home</a>
+            <a href="" className="nav-link active">About Us</a>
+            <a href="" className="nav-link">Gallery</a>
+            <a href="" className="nav-link">Blogs</a>
+            <a href="" className="nav-link">Contact Us</a>
+            </ul>
+           </div>
         </div>
       </div>
     </>
