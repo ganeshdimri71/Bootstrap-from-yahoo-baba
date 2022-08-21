709e464c36cfd64ec95127690a8b9ffac26fa8cd
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-21 12:49:14 +0530

    border and shadow

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -4,61 +4,211 @@ function App() {
   return (
     <>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">
-          embed-responsive embed-responsive-16by9
-        </h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <div className="embed-responsive embed-responsive-16by9">
-              <iframe
-                className="embed-responsive-item"
-                src="https://www.youtube.com/embed/r3LiyKA4gqs"
-              ></iframe>
+        <h2 className="py-3 text-center">Border and Shadow classes</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
             </div>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">
-          embed-responsive embed-responsive-21by9
-        </h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <div className="embed-responsive embed-responsive-4by9">
-              <iframe
-                className="embed-responsive-item"
-                src="https://www.youtube.com/embed/r3LiyKA4gqs"
-              ></iframe>
+        <h2 className="py-3 text-center">Border and Shadow classes</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border-top p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
             </div>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">
-          embed-responsive embed-responsive-21by9
-        </h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <div className="embed-responsive embed-responsive-21by9">
-              <iframe
-                className="embed-responsive-item"
-                src="https://www.youtube.com/embed/r3LiyKA4gqs"
-              ></iframe>
+        <h2 className="py-3 text-center">Border and Shadow classes</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border-start p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
             </div>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">
-          embed-responsive embed-responsive-1by1
-        </h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <div className="embed-responsive embed-responsive-1by1">
-              <iframe
-                className="embed-responsive-item"
-                src="https://www.youtube.com/embed/r3LiyKA4gqs"
-              ></iframe>
+        <h2 className="py-3 text-center">Border Top</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border border-top-0 p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">border-bottom-0</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border border-bottom-0 p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">border-end-0 </h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border border-end-0 p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">border-0 </h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border-0 p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">border-primary</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border border-primary p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">border-danger</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border border-danger p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">border-danger</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box" className="border border-danger p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Shadow</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box-shadow" className="shadow p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Shadow-sm</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box-shadow" className="shadow-sm p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Shadow-lg</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box-shadow" className="shadow-lg p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Shadow-nome</h2>
+        <div className="row py-5">
+          <div className="col-md-4 mx-auto">
+            <div id="box-shadow" className="shadow-none p-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                sapiente quod iure corporis nisi exercitationem accusamus!
+                Assumenda commodi sapiente iure temporibus, recusandae,
+                veritatis earum porro expedita alias ipsa, distinctio dolores?
+              </p>
             </div>
           </div>
         </div>
