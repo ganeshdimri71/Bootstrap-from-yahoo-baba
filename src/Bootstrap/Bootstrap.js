62a52c5da2b59b6ce8e0fc386145f7139e2a43e1
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-21 12:23:40 +0530

    video

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -4,148 +4,62 @@ function App() {
   return (
     <>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Images and Video</h2>
-        <div className="row py-3">
-          <div className="col-md-4" style={{ border: "4px solid red" }}>
-            <img src="src\assets\scenary.jpg" alt="road" />
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">img-fluid</h2>
-        <div className="row py-3">
-          <div className="col-md-4" style={{ border: "4px solid red" }}>
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid"
-              alt="road"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">img-fluid</h2>
+        <h2 className="py-3 text-center">
+          embed-responsive embed-responsive-16by9
+        </h2>
         <div className="row py-3">
           <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid img-thumbnail"
-              alt="road"
-            />
+            <div className="embed-responsive embed-responsive-16by9">
+              <iframe
+                className="embed-responsive-item"
+                src="https://www.youtube.com/embed/r3LiyKA4gqs"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">img-fluid rounded</h2>
+        <h2 className="py-3 text-center">
+          embed-responsive embed-responsive-21by9
+        </h2>
         <div className="row py-3">
           <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid rounded"
-              alt="road"
-            />
+            <div className="embed-responsive embed-responsive-4by9">
+              <iframe
+                className="embed-responsive-item"
+                src="https://www.youtube.com/embed/r3LiyKA4gqs"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">img-fluid rounded</h2>
+        <h2 className="py-3 text-center">
+          embed-responsive embed-responsive-21by9
+        </h2>
         <div className="row py-3">
           <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid rounded-lg"
-              alt="road"
-            />
+            <div className="embed-responsive embed-responsive-21by9">
+              <iframe
+                className="embed-responsive-item"
+                src="https://www.youtube.com/embed/r3LiyKA4gqs"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">img-fluid rounded-sm</h2>
+        <h2 className="py-3 text-center">
+          embed-responsive embed-responsive-1by1
+        </h2>
         <div className="row py-3">
           <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid rounded-sm"
-              alt="road"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">img-fluid rounded-circle</h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid rounded-circle"
-              alt="road"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">rounded-top</h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid rounded-top"
-              alt="road"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">rounded-top</h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid rounded-bottom"
-              alt="road"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">rounded-top</h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <img
-              src="src\assets\scenary.jpg"
-              className="img-fluid rounded-left"
-              alt="road"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">Image with text</h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <figure className="figure">
-              <img
-                src="src\assets\scenary.jpg"
-                className="figure-img img-fluid"
-                alt="road"
-              />
-              <figcaption className="figure-caption">Some text goes here</figcaption>
-            </figure>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">Image with text</h2>
-        <div className="row py-3">
-          <div className="col-md-4">
-            <figure className="figure">
-              <img
-                src="src\assets\scenary.jpg"
-                className="figure-img img-fluid"
-                alt="road"
-              />
-              <figcaption className="figure-caption text-end">Some text goes here</figcaption>
-            </figure>
+            <div className="embed-responsive embed-responsive-1by1">
+              <iframe
+                className="embed-responsive-item"
+                src="https://www.youtube.com/embed/r3LiyKA4gqs"
+              ></iframe>
+            </div>
           </div>
         </div>
       </div>
