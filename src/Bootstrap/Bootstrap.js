c938f070a62552a50ba3cbd980ff6ae086954c1f
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-21 15:43:20 +0530

    Spinners

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -6,28 +6,56 @@ function App() {
       <div className="container mb-5">
         <h2 className="py-3 text-center">Grouping Card</h2>
         <div className="row py-5">
+          <div className="col-6 text-center">
+            <div className="spinner-border text-primary  m-5"></div>
+          </div>
+          <div className="col-6 text-center">
+            <div className="d-flex justify-content-center">
+              <div className="spinner-border text-primary  m-5"></div>
+            </div>
+          </div>
+        </div>
+        <div className="row py-5 clearfix">
+          <div className="col-6 text-center">
+            <div className="spinner-border text-primary  m-5 float-start"></div>
+          </div>
+          <div className="col-6 text-center">
+            <div className="d-flex justify-content-center">
+              <div className="spinner-border text-primary  m-5 float-end"></div>
+            </div>
+          </div>
+        </div>
+        <div className="row py-5">
           <div className="col">
-            <h5>HTML</h5>
-            <div className="progress mb-2">
-              <div className="progress-bar" style={{ width: "25%" }}>
-                25%
-              </div>
-              <div className="progress-bar bg-danger" style={{ width: "15%" }}>
-                15%
-              </div>
-              <div className="progress-bar bg-info" style={{ width: "18%" }}>
-                18%
-              </div>
-            </div>
-            <h5>Web Designing</h5>
-            <div className="progress mb-2" style={{ height: "20px" }}>
-              <div
-                className="progress-bar progress-bar-striped progress-bar-animated bg-success"
-                style={{ width: "50%" }}
-              >
-                50%
-              </div>
-            </div>
+            <div className="spinner-border text-primary"></div>
+            <div className="spinner-border text-success"></div>
+            <div className="spinner-border spinner-border-sm text-danger"></div>
+            <div
+              className="spinner-border  text-info"
+              style={{ width: "80px", height: "80px" }}
+            ></div>
+          </div>
+        </div>
+        <div className="row py-5">
+          <div className="col">
+            <div className="spinner-grow text-primary"></div>
+            <div className="spinner-grow text-success"></div>
+            <div className="spinner-grow spinner-grow-sm text-danger"></div>
+            <div
+              className="spinner-grow  text-danger"
+              style={{ width: "80px", height: "80px" }}
+            ></div>
+          </div>
+        </div>
+        <div className="row py-5">
+          <div className="col">
+            <button className="btn btn-primary disabled">
+              <span className="spinner-border spinner-border-sm"></span>
+              Loading...
+            </button>
+            <button className="btn btn-primary disabled">
+              <span className="spinner-border spinner-border-sm"></span>
+            </button>
           </div>
         </div>
       </div>
