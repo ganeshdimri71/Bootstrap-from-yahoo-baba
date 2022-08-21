d1e765aab014c30b4914e77ad0b0fb1076e5d7a4
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-21 12:13:32 +0530

    image

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -4,135 +4,149 @@ function App() {
   return (
     <>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes</h2>
+        <h2 className="py-3 text-center">Bootstrap Images and Video</h2>
         <div className="row py-3">
-          <div className="col">
-            <ul className="nav">
-              <li className="nav-item">
-                <a href="" className="nav-link">Home</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link">About</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link">Services</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link">Contact Us</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link">Gallery</a>
-              </li>
-            </ul>
-           </div>
+          <div className="col-md-4" style={{ border: "4px solid red" }}>
+            <img src="src\assets\scenary.jpg" alt="road" />
+          </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes Active Disabled styling</h2>
+        <h2 className="py-3 text-center">img-fluid</h2>
         <div className="row py-3">
-          <div className="col">
-            <ul className="nav">
-              <li className="nav-item">
-                <a href="" className="nav-link active">Home</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link">About</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link">Services</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link">Contact Us</a>
-              </li>
-              <li className="nav-item">
-                <a href="" className="nav-link disabled">Gallery</a>
-              </li>
-            </ul>
-           </div>
+          <div className="col-md-4" style={{ border: "4px solid red" }}>
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid"
+              alt="road"
+            />
+          </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes Active Disabled</h2>
+        <h2 className="py-3 text-center">img-fluid</h2>
         <div className="row py-3">
-          <div className="col">
-            <nav className="nav">
-            <a href="" className="nav-link">Home</a>
-            <a href="" className="nav-link active">About Us</a>
-            <a href="" className="nav-link">Gallery</a>
-            <a href="" className="nav-link">Blogs</a>
-            <a href="" className="nav-link">Contact Us</a>
-            </nav>
-           </div>
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid img-thumbnail"
+              alt="road"
+            />
+          </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes Center align</h2>
-        <div className="row py-5">
-          <div className="col">
-            <nav className="nav justify-content-center">
-            <a href="" className="nav-link">Home</a>
-            <a href="" className="nav-link active">About Us</a>
-            <a href="" className="nav-link">Gallery</a>
-            <a href="" className="nav-link">Blogs</a>
-            <a href="" className="nav-link">Contact Us</a>
-            </nav>
-           </div>
+        <h2 className="py-3 text-center">img-fluid rounded</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid rounded"
+              alt="road"
+            />
+          </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes right align</h2>
-        <div className="row py-5">
-          <div className="col">
-            <nav className="nav justify-content-end">
-            <a href="" className="nav-link">Home</a>
-            <a href="" className="nav-link active">About Us</a>
-            <a href="" className="nav-link">Gallery</a>
-            <a href="" className="nav-link">Blogs</a>
-            <a href="" className="nav-link">Contact Us</a>
-            </nav>
-           </div>
+        <h2 className="py-3 text-center">img-fluid rounded</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid rounded-lg"
+              alt="road"
+            />
+          </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes Menu Vertical</h2>
-        <div className="row py-5">
-          <div className="col">
-            <nav className="nav flex-column">
-            <a href="" className="nav-link">Home</a>
-            <a href="" className="nav-link active">About Us</a>
-            <a href="" className="nav-link">Gallery</a>
-            <a href="" className="nav-link">Blogs</a>
-            <a href="" className="nav-link">Contact Us</a>
-            </nav>
-           </div>
+        <h2 className="py-3 text-center">img-fluid rounded-sm</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid rounded-sm"
+              alt="road"
+            />
+          </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes Nav Pills</h2>
-        <div className="row py-5">
-          <div className="col">
-            <ul className="nav nav-pills">
-            <a href="" className="nav-link">Home</a>
-            <a href="" className="nav-link active">About Us</a>
-            <a href="" className="nav-link">Gallery</a>
-            <a href="" className="nav-link">Blogs</a>
-            <a href="" className="nav-link">Contact Us</a>
-            </ul>
-           </div>
+        <h2 className="py-3 text-center">img-fluid rounded-circle</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid rounded-circle"
+              alt="road"
+            />
+          </div>
         </div>
       </div>
       <div className="container mb-4">
-        <h2 className="py-3 text-center">Bootstrap Nav Classes Nav Pills And Nav Fills</h2>
-        <div className="row py-5">
-          <div className="col">
-            <ul className="nav nav-pills nav-fill">
-            <a href="" className="nav-link">Home</a>
-            <a href="" className="nav-link active">About Us</a>
-            <a href="" className="nav-link">Gallery</a>
-            <a href="" className="nav-link">Blogs</a>
-            <a href="" className="nav-link">Contact Us</a>
-            </ul>
-           </div>
+        <h2 className="py-3 text-center">rounded-top</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid rounded-top"
+              alt="road"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">rounded-top</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid rounded-bottom"
+              alt="road"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">rounded-top</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <img
+              src="src\assets\scenary.jpg"
+              className="img-fluid rounded-left"
+              alt="road"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Image with text</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <figure className="figure">
+              <img
+                src="src\assets\scenary.jpg"
+                className="figure-img img-fluid"
+                alt="road"
+              />
+              <figcaption className="figure-caption">Some text goes here</figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div className="container mb-4">
+        <h2 className="py-3 text-center">Image with text</h2>
+        <div className="row py-3">
+          <div className="col-md-4">
+            <figure className="figure">
+              <img
+                src="src\assets\scenary.jpg"
+                className="figure-img img-fluid"
+                alt="road"
+              />
+              <figcaption className="figure-caption text-end">Some text goes here</figcaption>
+            </figure>
+          </div>
         </div>
       </div>
     </>
