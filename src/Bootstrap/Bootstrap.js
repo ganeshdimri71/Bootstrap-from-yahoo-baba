9b47ccc510ae2e537451b93f71a3a67e55f8be79
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-21 15:25:15 +0530

    Progressbar

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -7,37 +7,27 @@ function App() {
         <h2 className="py-3 text-center">Grouping Card</h2>
         <div className="row py-5">
           <div className="col">
-            <h1>
-              Example heading <span class="badge bg-secondary">New</span>
-            </h1>
-            <h2>
-              Example heading{" "}
-              <span class="badge rounded-pill text-bg-primary">New</span>
-            </h2>
-            <button className="btn btn-primary">
-              Notifications <span className="badge text-bg-light">5</span>{" "}
-            </button>
-            <a href="" className="badge text-decoration-none bg-info">
-              Read More
-            </a>
-            <a
-              href=""
-              className="badge bg-danger text-decoration-none rounded-pill p-2"
-            >
-              Read More
-            </a>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-              pariatur nobis perferendis vero vitae quia ipsa consectetur rerum
-              perspiciatis span soan{" "}
-              <span className="badge text-bg-warning">
-                {" "}
-                suscipit cumque unde veniam, deserunt{" "}
-              </span>{" "}
-              quaerat, modi adipisci dolor molestias praesentium necessitatibus
-              reprehenderit. Earum, nisi hic ratione ab voluptatibus pariatur
-              dignissimos ducimus eligendi totam magnam impedit doloremque.
-            </p>
+            <h5>HTML</h5>
+            <div className="progress mb-2">
+              <div className="progress-bar" style={{ width: "25%" }}>
+                25%
+              </div>
+              <div className="progress-bar bg-danger" style={{ width: "15%" }}>
+                15%
+              </div>
+              <div className="progress-bar bg-info" style={{ width: "18%" }}>
+                18%
+              </div>
+            </div>
+            <h5>Web Designing</h5>
+            <div className="progress mb-2" style={{ height: "20px" }}>
+              <div
+                className="progress-bar progress-bar-striped progress-bar-animated bg-success"
+                style={{ width: "50%" }}
+              >
+                50%
+              </div>
+            </div>
           </div>
         </div>
       </div>
