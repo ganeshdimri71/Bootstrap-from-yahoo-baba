1844529382a6029fb0f6f287daedf9fed07de47d
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-21 17:21:07 +0530

    popover

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 import Bootstrap from "./Bootstrap/Bootstrap";
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-   <Bootstrap />
-  )
+  // $(function () {
+  //   $('[data-bs-toggle="popover"]').popover();
+  // });
+
+  // $(function () {
+  //   $("#example").popover({
+  //     html: true,
+  //     title: "Popover",
+  //     content:
+  //       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, numquam?",
+  //     placement: "bottom",
+  //   });
+
+  //   $("#showbtn").click(function () {
+  //     $("#example").popover("toggle");
+  //   });
+  //   $("#hidebtn").click(function () {
+  //     $("#example").popover("hide");
+  //   });
+  //   $("#example").on("show.bs.popover", function () {
+  //     alert("Show ");
+  //   });
+  //   $("#example").on("shown.bs.popover", function () {
+  //     alert("Shown ");
+  //   });
+  //   $("#example").on("hide.bs.popover", function () {
+  //     alert("Hide ");
+  //   });
+  //   $("#example").on("hidden.bs.popover", function () {
+  //     alert("hidden ");
+  //   });
+  //   // $("#example").on("hidde.bs.popover", function () {
+  //   //   alert("hidden ");
+  //   // });
+  // });
+
+  return <Bootstrap />;
 }
 
-export default App
+export default App;
--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -1,66 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+// const popoverTriggerList = document.querySelectorAll(
+//   '[data-bs-toggle="popover"]'
+// );
+// const popoverList = [...popoverTriggerList].map(
+//   (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+// );
+function Bootstrap() {
+  const popoverRef = useRef();
+  useEffect(() => {
+    var popover = new bootstrap.Popover(popoverRef.current, {
+      content: "Hello popover content!",
+      title: "My Popover",
+    });
+  });
 
-function App() {
   return (
-    <>
-      <div className="container mb-5">
-        <h2 className="py-3 text-center">Grouping Card</h2>
-        <div className="row py-5">
-          <div className="col-6 text-center">
-            <div className="spinner-border text-primary  m-5"></div>
-          </div>
-          <div className="col-6 text-center">
-            <div className="d-flex justify-content-center">
-              <div className="spinner-border text-primary  m-5"></div>
-            </div>
-          </div>
-        </div>
-        <div className="row py-5 clearfix">
-          <div className="col-6 text-center">
-            <div className="spinner-border text-primary  m-5 float-start"></div>
-          </div>
-          <div className="col-6 text-center">
-            <div className="d-flex justify-content-center">
-              <div className="spinner-border text-primary  m-5 float-end"></div>
-            </div>
-          </div>
-        </div>
-        <div className="row py-5">
-          <div className="col">
-            <div className="spinner-border text-primary"></div>
-            <div className="spinner-border text-success"></div>
-            <div className="spinner-border spinner-border-sm text-danger"></div>
-            <div
-              className="spinner-border  text-info"
-              style={{ width: "80px", height: "80px" }}
-            ></div>
-          </div>
-        </div>
-        <div className="row py-5">
-          <div className="col">
-            <div className="spinner-grow text-primary"></div>
-            <div className="spinner-grow text-success"></div>
-            <div className="spinner-grow spinner-grow-sm text-danger"></div>
-            <div
-              className="spinner-grow  text-danger"
-              style={{ width: "80px", height: "80px" }}
-            ></div>
-          </div>
-        </div>
-        <div className="row py-5">
-          <div className="col">
-            <button className="btn btn-primary disabled">
-              <span className="spinner-border spinner-border-sm"></span>
-              Loading...
-            </button>
-            <button className="btn btn-primary disabled">
-              <span className="spinner-border spinner-border-sm"></span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="p-4">
+      <button className="btn btn-lg btn-danger" ref={popoverRef}>
+        Click to toggle popover
+      </button>
+    </div>
   );
 }
 
-export default App;
+export default Bootstrap;
