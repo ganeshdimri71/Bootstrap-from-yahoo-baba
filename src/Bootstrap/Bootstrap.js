fbf5d0c95eb8d96b25a0d1a85f067364afbc4467
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-08-21 15:11:50 +0530

    Badges

--- src/Bootstrap/Bootstrap.js
+++ src/Bootstrap/Bootstrap.js
@@ -3,240 +3,41 @@ import React from "react";
 function App() {
   return (
     <>
-      <div className="container mb-4">
+      <div className="container mb-5">
         <h2 className="py-3 text-center">Grouping Card</h2>
         <div className="row py-5">
-          <div className="col mb-2">
-            <div className="card-group">
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Iusto quo molestias eius optio nostrum magnam corrupti
-                    laboriosam omnis dicta ea?
-                  </p>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Iusto quo molestias eius optio nostrum magnam corrupti
-                    laboriosam omnis dicta ea?
-                  </p>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Iusto quo molestias eius optio nostrum magnam corrupti
-                    laboriosam omnis dicta ea?
-                  </p>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Iusto quo molestias eius optio nostrum magnam corrupti
-                    laboriosam omnis dicta ea?
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">Crad Deck Working Layout</h2>
-        <div className="row row-cols-1 row-cols-md-4 mb-3">
-          {/* <div className="card-group"> */}
           <div className="col">
-            <div className="card mb-2">
-              <img
-                src="src\assets\scenary.jpg"
-                alt="Crad-Image"
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <h4 className="card-title">Card Text Center</h4>
-                <p className="card-text">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Iusto quo molestias eius optio nostrum magnam corrupti
-                  laboriosam omnis dicta ea?
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card mb-2">
-              <img
-                src="src\assets\scenary.jpg"
-                alt="Crad-Image"
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <h4 className="card-title">Card Text Center</h4>
-                <p className="card-text">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Iusto quo molestias eius optio nostrum magnam corrupti
-                  laboriosam omnis dicta ea?
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card mb-2">
-              <img
-                src="src\assets\scenary.jpg"
-                alt="Crad-Image"
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <h4 className="card-title">Card Text Center</h4>
-                <p className="card-text">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Iusto quo molestias eius optio nostrum magnam corrupti
-                  laboriosam omnis dicta ea?
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card mb-2">
-              <img
-                src="src\assets\scenary.jpg"
-                alt="Crad-Image"
-                className="card-img-top"
-              />
-              <div className="card-body">
-                <h4 className="card-title">Card Text Center</h4>
-                <p className="card-text">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Iusto quo molestias eius optio nostrum magnam corrupti
-                  laboriosam omnis dicta ea?
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* </div> */}
-        </div>
-      </div>
-      <div className="container mb-4">
-        <h2 className="py-3 text-center">Grouping Card</h2>
-        <div className="row py-5">
-          <div className="col mb-2">
-            <div className="card-columns">
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Iusto quo molestias eius optio nostrum magnam corrupti
-                    laboriosam omnis dicta ea?
-                  </p>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  </p>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing Lorem
-                    ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-                    consequuntur? Lorem ipsum dolor sit amet.
-                  </p>
-                  <button className="btn btn-primary">Click Here</button>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Iusto quo molestias eius optio nostrum magnam corrupti
-                    laboriosam omnis dicta ea?
-                  </p>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing amet.
-                  </p>
-                  <button className="btn btn-primary">Click Here</button>
-                </div>
-              </div>
-              <div className="card mb-2">
-                <img
-                  src="src\assets\scenary.jpg"
-                  alt="Crad-Image"
-                  className="card-img-top"
-                />
-                <div className="card-body">
-                  <h4 className="card-title">Card Text Center</h4>
-                  <p className="card-text">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing Lorem
-                    ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-                    consequuntur? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio, aut.
-                  </p>
-                  <button className="btn btn-primary">Click Here</button>
-                </div>
-              </div>
-            </div>
+            <h1>
+              Example heading <span class="badge bg-secondary">New</span>
+            </h1>
+            <h2>
+              Example heading{" "}
+              <span class="badge rounded-pill text-bg-primary">New</span>
+            </h2>
+            <button className="btn btn-primary">
+              Notifications <span className="badge text-bg-light">5</span>{" "}
+            </button>
+            <a href="" className="badge text-decoration-none bg-info">
+              Read More
+            </a>
+            <a
+              href=""
+              className="badge bg-danger text-decoration-none rounded-pill p-2"
+            >
+              Read More
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+              pariatur nobis perferendis vero vitae quia ipsa consectetur rerum
+              perspiciatis span soan{" "}
+              <span className="badge text-bg-warning">
+                {" "}
+                suscipit cumque unde veniam, deserunt{" "}
+              </span>{" "}
+              quaerat, modi adipisci dolor molestias praesentium necessitatibus
+              reprehenderit. Earum, nisi hic ratione ab voluptatibus pariatur
+              dignissimos ducimus eligendi totam magnam impedit doloremque.
+            </p>
           </div>
         </div>
       </div>
